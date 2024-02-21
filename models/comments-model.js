79b390d0f823f72846b2e64fe07ac289d7d33cd2
Alice Georgina <alicewilson1@Alices-MBP.lan>
2024-02-21 15:13:34 +0000

    completed 07-post-comments-for-article

--- models/comments-model.js
+++ models/comments-model.js
@@ -9,3 +9,12 @@ return db.query(`SELECT * FROM comments WHERE article_id = $1 ORDER BY created_a
 return result.rows
 })
 }
+
+exports.addCommentToArticle = (article_id, commentData) => {
+const {username, body} = commentData;
+    return db.query(`INSERT INTO comments (author, body, article_id, votes) VALUES ($1, $2, $3, 0) RETURNING *;`, [username, body, article_id])
+    .then((result) => {
+        return result.rows[0]
+    
+    })
+} 
