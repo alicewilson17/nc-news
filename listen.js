59f57bf78b9c2f5fd391989ef47422c5f5af9c19
Alice Georgina <alicewilson1@Alices-MBP.lan>
2024-02-23 11:47:02 +0000

    hosting set up

--- listen.js
+++ listen.js
@@ -1,9 +1,10 @@
 const app = require('./app')
+const {PORT = 9090} = process.env
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
     if (err) {
         console.log(err)
     } else {
-        console.log("listening on 9090")
+        console.log(`Listening on ${PORT}...`)
     }
 })
